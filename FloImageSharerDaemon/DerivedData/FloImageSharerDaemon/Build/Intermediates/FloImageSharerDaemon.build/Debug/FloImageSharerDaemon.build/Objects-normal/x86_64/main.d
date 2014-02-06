1a66c65e59742982ecb6cbdc91fe4e34047231c4
AlexanderC <alexander.cucer@flosites.com>
2014-02-06 16:11:30 +0200

    rebase proj

--- FloImageSharerDaemon/DerivedData/FloImageSharerDaemon/Build/Intermediates/FloImageSharerDaemon.build/Debug/FloImageSharerDaemon.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/User/Desktop/Projects/FloImageSharerDaemon/FloImageSharerDaemon/FloImageSharerDaemon/FloImageSharerDaemon/main.cpp \
-  /Users/User/Desktop/Projects/FloImageSharerDaemon/FloImageSharerDaemon/FloImageSharerDaemon/FloImageSharerDaemon/sharer.h
--- FloImageSharerDaemon/DerivedData/FloImageSharerDaemon/Build/Intermediates/FloImageSharerDaemon.build/Debug/FloImageSharerDaemon.build/Objects-normal/x86_64/sharer.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/User/Desktop/Projects/FloImageSharerDaemon/FloImageSharerDaemon/FloImageSharerDaemon/FloImageSharerDaemon/sharer.cpp \
-  /Users/User/Desktop/Projects/FloImageSharerDaemon/FloImageSharerDaemon/FloImageSharerDaemon/FloImageSharerDaemon/sharer.h
